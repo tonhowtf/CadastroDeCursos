41e555fd9b4a8a44f2d43403f35d5f24b51935fa
Tonho <antonioreverso.com@gmail.com>
2025-04-09 18:37:42 -0300

    first commit

---
+++ src/main/java/wtf/tonho/CadastroDeCursos/CadastroDeCursosApplication.java
@@ -0,0 +1,13 @@
+package wtf.tonho.CadastroDeCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wtf/tonho/CadastroDeCursos/CadastroDeCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.tonho.CadastroDeCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
