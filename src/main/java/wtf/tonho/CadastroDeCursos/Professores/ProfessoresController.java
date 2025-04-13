8eaa8e74146c8315af515c1ffddb56e3d6886812
Tonho <antonioreverso.com@gmail.com>
2025-04-13 12:24:44 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresController.java
+++ src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresController.java
@@ -1,4 +1,35 @@
 package wtf.tonho.CadastroDeCursos.Professores;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("professores")
 public class ProfessoresController {
+
+    @PostMapping("/criar")
+    public String criarProfessor() {
+        return "Criado com sucesso";
+    }
+
+    @GetMapping("/todos")
+    public String listarProfessores() {
+        return "Lista de professores";
+    }
+
+    @GetMapping("/todosID")
+    public String professoresPorId() {
+        return "Lista de professores";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarProfessorPorID() {
+        return "Alterado com sucesso";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarProfessorPorID() {
+        return "Deletado com sucesso";
+    }
+
+
 }
