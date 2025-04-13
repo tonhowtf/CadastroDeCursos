4938dc78c11cd3f8324a20c9058f2cde5f046af3
Tonho <antonioreverso.com@gmail.com>
2025-04-13 11:07:54 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/wtf/tonho/CadastroDeCursos/Cursos/CursoController.java
+++ src/main/java/wtf/tonho/CadastroDeCursos/Cursos/CursoController.java
@@ -1,9 +1,7 @@
 package wtf.tonho.CadastroDeCursos.Cursos;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -13,5 +11,33 @@ public class CursoController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+
+    // Adicionar Curso (Create)
+    @PostMapping("/criar")
+    public String criarCurso() {
+        return "Criado com sucesso";
+    }
+
+    // Mostrar todos os Cursos (read)
+    @GetMapping("/todos")
+    public String listarCursos() {
+        return "Lista de cursos";
+    }
+    // Procurar Curso por ID (Read)
+    @GetMapping("/todosID")
+    public String cursoPorId() {
+        return "Lista de cursos";
+    }
+    // Alterar dados dos Cursos (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarCursoPorID() {
+        return "Alterado com sucesso";
+    }
+    // Deletar Curso (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarCursoPorID() {
+        return "Deletado com sucesso";
     }
 }
