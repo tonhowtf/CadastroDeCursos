d693848201464041b4201fa518178483926894c9
Tonho <antonioreverso.com@gmail.com>
2025-04-14 15:46:44 -0300

    feat: endpoints

--- src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresController.java
+++ src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresController.java
@@ -1,34 +1,44 @@
 package wtf.tonho.CadastroDeCursos.Professores;
 
 import org.springframework.web.bind.annotation.*;
+import wtf.tonho.CadastroDeCursos.Cursos.CursoModel;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/professores")
 public class ProfessoresController {
 
+    private final ProfessoresRepository professoresRepository;
+    private ProfessoresService professoresService;
+
+    public ProfessoresController(ProfessoresService professoresService, ProfessoresRepository professoresRepository) {
+        this.professoresService = professoresService;
+        this.professoresRepository = professoresRepository;
+    }
+
     @PostMapping("/criar")
-    public String criarProfessor() {
-        return "Criado com sucesso";
+    public ProfessoresModel criarProfessor(@RequestBody ProfessoresModel professor) {
+        return professoresRepository.save(professor);
     }
 
     @GetMapping("/listar")
-    public String listarProfessores() {
-        return "Lista de professores";
+    public List<ProfessoresModel> listarProfessores() {
+        return professoresService.listarProfessores();
     }
 
-    @GetMapping("/listarID")
-    public String professoresPorId() {
-        return "Lista de professores";
+    @GetMapping("/listar/{id}")
+    public ProfessoresModel professoresPorId(@PathVariable Long id) {
+        return professoresService.listarProfessoresPorId(id);
     }
 
-    @PutMapping("/alterarID")
-    public String alterarProfessorPorID() {
-        return "Alterado com sucesso";
+    @PutMapping("/alterar/{id}")
+    public ProfessoresModel alterarProfessorPorId(@PathVariable Long id, @RequestBody ProfessoresModel professor) {
+        return professoresService.alterarProfessorPorId(professor, id);
     }
-
-    @DeleteMapping("/deletarID")
-    public String deletarProfessorPorID() {
-        return "Deletado com sucesso";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarProfessorPorID(@PathVariable Long id) {
+          professoresRepository.deleteById(id);
     }
 
 
--- src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresService.java
+++ src/main/java/wtf/tonho/CadastroDeCursos/Professores/ProfessoresService.java
@@ -1,4 +1,46 @@
 package wtf.tonho.CadastroDeCursos.Professores;
 
+import org.springframework.stereotype.Service;
+import wtf.tonho.CadastroDeCursos.Cursos.CursoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProfessoresService {
+
+    private ProfessoresRepository professoresRepository;
+
+    public ProfessoresService(ProfessoresRepository professoresRepository) {
+        this.professoresRepository = professoresRepository;
+    }
+
+    public ProfessoresModel criarProfessor(ProfessoresModel professor){
+        return professoresRepository.save(professor);
+    }
+
+    public List<ProfessoresModel> listarProfessores(){
+        return professoresRepository.findAll();
+    }
+
+
+    public ProfessoresModel listarProfessoresPorId(Long id){
+        Optional<ProfessoresModel> professorPorId = professoresRepository.findById(id);
+        return professorPorId.orElse(null);
+    }
+
+    public ProfessoresModel alterarProfessorPorId(ProfessoresModel professor, Long id) {
+        if(professoresRepository.existsById(id)){
+            professor.setId(id);
+            return professoresRepository.save(professor);
+        }
+        return null;
+    }
+
+    public void deletarProfessorPorId(Long id) {
+        professoresRepository.deleteById(id);
+    }
+
+
 }
